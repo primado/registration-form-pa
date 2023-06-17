7e2a717a02e0cb78e8c2f00c4d513270d99adf0f
primado <primadoprincepa@gmail.com>
2023-06-17 17:52:59 +0000

    made sucess responsive

--- src/component/Success.jsx
+++ src/component/Success.jsx
@@ -6,11 +6,11 @@ export default function Success() {
 
     return (
         <div className="success font-serif bg-gray-300 min-h-screen">
-            <div className="flex justify-center items-center py-[10rem]">
-                <div className="max-w-[30rem] bg-white   rounded-xl  border-2 border-black">
+            <div className="flex justify-center items-center py-[10rem] sm-425:py-0 sm-375:py-0 sm-320:py-0">
+                <div className="max-w-[30rem] bg-white rounded-xl shadow-2xl sm-425:min-h-screen sm-425:rounded-none">
                     <div className="">
-                        <div className="description px-5 py-5">
-                            <div className="flex flex-col">
+                        <div className="description px-5 py-5 sm-425:py-[10rem] sm-375:py-[10rem] sm-320:py-[10rem]">
+                            <div className="flex flex-col ">
                                 <img 
                                     src={check} 
                                     alt=" success check icon"
